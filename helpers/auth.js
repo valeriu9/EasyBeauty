29b50e410cd1c9bdc524243c14f387cd059e267a
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2021-05-20 18:27:35 +0200

    remove old

--- helpers/auth.js
+++
@@ -1,7 +0,0 @@
-export function initializeUserData (store) {
-  const userIsLoggedIn = $cookies.get('movie_user');
-
-  if (userIsLoggedIn) {
-    store.dispatch('user/userLoggedIn', $cookies.get('movie_user'));
-  }
-}
