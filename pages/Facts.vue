bcc2626f59419b3d7cf7dc0f47f1edd0fd1517cf
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2021-06-02 10:17:51 +0200

    Update Facts.vue

--- pages/Facts.vue
+++ pages/Facts.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="container">
-    <Linechart v-if="getRating" :chartdata="getRating" />
-    <BarChart v-if="getYears" :chartdata="getYears" />
+  <div>
+    <div v-if="loading" class="loader"></div>
+    <div class="container">
+      <Linechart v-if="getRating" :chartdata="getRating" />
+      <BarChart v-if="getYears" :chartdata="getYears" />
 
+    </div>
   </div>
 </template>
 
@@ -15,18 +18,21 @@ export default {
   },
   data(){
     return{
+      loading: false,
       getRating:null,
       getYears:null
       }
   },
   methods:{
    async fetchData(){
+      this.loading = true
         try{
      const res = await this.$axios.get('https://europe-west1-sep6-314214.cloudfunctions.net/getRating');
      const res2 = await this.$axios.get('https://europe-west1-sep6-314214.cloudfunctions.net/get_last20Years');
 
      this.transform(res.data)
      this.transform2(res2.data)
+     this.loading = false
    }
    catch(e){
      console.log(e);
@@ -58,5 +64,26 @@ export default {
 .container {
   background-color: rgba(235, 235, 235, 0.905);
   margin-top: 30px;
+}
+.loader {
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  width: 36px;
+  height: 36px;
+  animation: spin 2s linear infinite;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
