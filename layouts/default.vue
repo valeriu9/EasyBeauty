c41f1d91bfa201edbe8a534bb842f2cfd8dd47a3
valeriu9 <valeriu.marandici98@gmail.com>
2021-10-14 13:30:13 +0200

    Initial commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,34 +2,17 @@
   <div>
     <client-only>
       <Navbar />
-      <Nuxt />
+      <nuxt />
     </client-only>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+<script>
+import Navbar from '~/components/Navbar';
 
-}
-body {
-  min-height: 100vh  !important;
-  background-image: url(~/assets/images/background.svg);
-}
-
-
-</style>
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
