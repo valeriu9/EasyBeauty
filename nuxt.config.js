64b07cb16bbad350fde48dfe92b493706fc834d9
valeriu9 <valeriu.marandici98@gmail.com>
2021-12-13 02:58:04 +0100

    handle mixed content

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ export default {
       lang: 'en',
     },
     meta: [
+      { httpEquiv:"Content-Security-Policy", content:"upgrade-insecure-requests" },
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
